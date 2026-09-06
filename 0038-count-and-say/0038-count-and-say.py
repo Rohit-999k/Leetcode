315a1a59d4e6aa9c72df70cf6f873cebd698e077
Rohit Sanodiya <officialrohit927@gmail.com>
2026-09-06 22:06:34 +0530

    Time: 11 ms (60.46%), Space: 12.5 MB (22.53%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,30 +1,25 @@
 class Solution(object):
     def countAndSay(self, n):
         s = "1"
-    
-        for i in range(1,n):
-            sol = ""
-            prev = s[0]
 
-            count =  0
-            j = 0
-            while(j<len(s)):
-                if prev == s[j]:
-                    count+=1
+        for _ in range(1 , n):
+            sol = []
+            count =1
+
+            for j in range(1 , len(s)):
+
+                if s[j]==s[j-1]:
+                    count += 1
+                    
                 else:
-                    sol += (str(count)+prev)
+                    sol.append(str(count))
+                    sol.append(s[j-1])
                     count = 1
-                    prev = s[j]
-                
-                j+=1
-            
-            sol += (str(count)+prev)
 
-            s = sol
-        
+            sol.append(str(count))
+            sol.append(s[-1])
+
+            s = "".join(sol)
+
         return s
 
-
-            
-
-        
